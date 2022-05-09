b18d62a67f382aeb5dfac6af3c55bd1c833df5d3
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-05-09 22:42:47 +0300

    Create main.go

---
+++ 05_zero_values/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var name string
+	fmt.Println(name) // string --> ""
+
+	var age int
+	fmt.Println(age) // numeric --> 0
+
+	var isMarried bool // bool --> false
+	
+	fmt.Println(isMarried)
+}
