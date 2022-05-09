120120e1ee568b96f0d0479cbae212aedfe139da
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-05-09 20:42:46 +0300

    Create main.go

---
+++ 02_variable_declaration/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+	var name string // var - name of variable - static type
+	name = "Baran"
+
+	var age int
+	age = 22
+
+	var isMarried bool
+	isMarried = false
+	*/
+
+	name := "Baran"
+	age := 22
+	isMarried := false
+	
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	
+}
