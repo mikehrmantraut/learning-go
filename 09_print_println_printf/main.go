a68159f156a32ed509f52739996bbbd046e93854
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-05-12 10:06:30 +0300

    Create main.go

---
+++ 09_print_println_printf/main.go
@@ -0,0 +1,55 @@
+package main
+
+/* var x = 10
+var customerName
+var Y = 100 */
+
+func main() {
+
+	// (Print - Println) - Printf
+
+	/* 	{
+		var c
+	} */
+
+	/* 	fmt.Println("Hello")
+	   	fmt.Print("Hello")
+	   	fmt.Println("")
+	   	fmt.Printf("Hello") */
+
+	/* 	name := "Baran" */
+
+	/* 	fmt.Print(name)
+	   	fmt.Println(name)
+	   	fmt.Printf(name) */
+
+	/* 	fmt.Print("My name is", name)
+	   	fmt.Println("")
+	   	fmt.Println("My name is", name)
+	   	fmt.Printf("My name is %X %T", name, name) */
+
+	/* 	x := 100
+	   	y := 20
+	   	z := 30
+	   	fmt.Printf("%b %d %o", x, y, z) */
+
+	/* name, age := "Baran", 22 */
+
+	// fmt.Print("My name is", name, ", and I am ", age, "years old.")
+	// fmt.Println("My name is", name, "and I am", age, "years old.")
+	// fmt.Printf("My name is %v, and I am %v years old.", name, age)
+
+	// VISIBILITY
+
+	/* 	fmt.Println(x)
+	   	myFunc() */
+
+	/* 	var coin string  // count - customer - coin
+	   	var coinType string
+	   	var custName string
+	   	// abbreviations are written in capital letters
+	   	var URL // NOT url
+	   	var HTTP // NOT http  "xyzHTTP"
+	   	i , j , k */
+
+}
