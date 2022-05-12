9a375316542f086366f00be7ae6a677662de8d56
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-05-12 15:13:26 +0300

    Update main.go

--- 17_conditional_statements_II/main.go
+++ 17_conditional_statements_II/main.go
@@ -1,1 +1,28 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	/* 	x := 10
+	   	if x := -5; x < 0 {
+	   		fmt.Println(x, "is an negative number.")
+	   	} else if x%2 == 0 {
+	   		fmt.Println(x, "is an even number.")
+	   	} else {
+	   		fmt.Println(x, "is an odd number.")
+	   	}
+		   fmt.Println(x) */
+
+	if x := -25; x < 0 {
+		fmt.Println(x, "is a negative number.")
+		fmt.Println("My name is Baran.")
+	} else {
+		if x%2 == 0 {
+			fmt.Println(x, "is an even number.")
+		} else {
+			fmt.Println(x, "is an odd number.")
+		}
+	}
+
+}
